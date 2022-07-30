3224a72b5a0bf4ec18bc76d134b95a1680db6a9c
mqberry11 <107665588+mqberry11@users.noreply.github.com>
2022-07-30 15:39:51 -0700

    Delete Drum Kit directory

--- Drum Kit/test.js
+++
@@ -1,7 +0,0 @@
-var numberOfDrumButtons = document.querySelectorAll(".drum").length;
-
-for (i = 0; i < numberOfDrumButtons; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    alert("Alternative Code Works Too!");
-  })
-}
